6f0445768060786251256dabd9f4e8639cdfbe4c
Daniel Cahn <cahnteam@gmail.com>
2020-02-14 09:56:20 +0000

    Add a library to use in notebooks

---
+++ daniel_lib.py
@@ -0,0 +1,240 @@
+from transformers import (BertTokenizer,
+                          BertModel,
+                          AutoTokenizer,
+                          AutoModel,
+                          AdamW,
+                          get_linear_schedule_with_warmup)
+import pandas as pd
+import torch
+from torch import nn
+import torch.nn.functional as F
+import numpy as np
+import scipy
+
+tokenizer = AutoTokenizer.from_pretrained("bert-base-multilingual-cased")
+bert_model = AutoModel.from_pretrained("bert-base-multilingual-cased")
+
+def set_device(_device):
+    device = _device
+
+def import_file(prefix):
+  with open(f"{prefix}.ende.src", "r", encoding="utf-8") as f:
+    src = [line.strip() for line in f]
+  with open(f"{prefix}.ende.mt", "r", encoding="utf-8") as f:
+    mt = [line.strip() for line in f]
+  scores = None
+  if(prefix!="test"):
+    with open(f"{prefix}.ende.scores", "r", encoding="utf-8") as f:
+      scores = [float(line.strip()) for line in f]
+  return pd.DataFrame({"src":src,"mt":mt, "scores":scores})
+
+def pad(id_sequences, wiggle_room=0, max_length=None):
+    if max_length is None:
+        max_length = max([len(s) for s in id_sequences]) + wiggle_room
+    padded_data = np.zeros((len(id_sequences), max_length))
+    for i, sample in enumerate(id_sequences):
+        padded_data[i, :len(sample)] = sample
+    return padded_data
+
+def get_tokenized(dataframe):
+  input1 = dataframe.apply(lambda a: "[CLS] "  + a.src + " [SEP] "+a.mt+" [SEP]", axis=1)\
+  .apply(lambda a: tokenizer.tokenize(a))\
+  .apply(lambda a: tokenizer.convert_tokens_to_ids(a))
+
+  input2 = dataframe.apply(lambda a: "[CLS] " + a.mt + " [SEP] "+a.src+" [SEP]", axis=1)\
+  .apply(lambda a: tokenizer.tokenize(a))\
+  .apply(lambda a: tokenizer.convert_tokens_to_ids(a))
+  return input1, input2
+
+def getDataLoader(dataframe, batch_size=32, test=False):
+  input1, input2 = get_tokenized(dataframe)
+  if test:
+    l = list(zip(pad(input1),pad(input2)))
+  else:
+    l = list(zip(pad(input1),pad(input2),dataframe.scores))
+
+  return torch.utils.data.DataLoader(l, batch_size=batch_size, shuffle=(not test))
+
+def removeOutliers(dataframe, negLimit=-3, posLimit=2):
+  dataframe.loc[dataframe.scores<negLimit,"scores"] = negLimit
+  dataframe.loc[dataframe.scores>posLimit,"scores"] = posLimit
+  return dataframe
+
+from IPython.display import HTML, display
+import time
+
+def progress(value, max=100):
+    return HTML("""
+        <progress
+            value='{value}'
+            max='{max}',
+            style='width: 100%'
+        >
+            {value}
+        </progress>
+    """.format(value=value, max=max))
+
+
+def get_sentence_embeddings(dataframe, bert_model, test=False, batch_size=32):
+    print("Tokenizing data...")
+    input1, input2 = get_tokenized(dataframe)
+
+    loader = torch.utils.data.DataLoader(list(zip(input1, input2)), batch_size=32)
+
+    bert_model.eval()
+    bert_model.to(device=device)
+
+    l = []
+    z = len(list(loader));
+
+    progress_bar = display(progress(0, z), display_id=True)
+
+    with torch.no_grad():
+        for i, (x1, x2) in enumerate(loader):
+            x1 = torch.LongTensor(pad(x1)).to(device=device, dtype=torch.long)
+            x2 = torch.LongTensor(pad(x2)).to(device=device, dtype=torch.long)
+            o1 = bert_model(x1)[1]
+            o2 = bert_model(x2)[1]
+
+            out = [(o1[i], o2[i]) for i in range(len(o1))]
+            l += out;
+
+            progress_bar.update(progress(i, z))
+
+    if not test:
+        l = list(zip(l, dataframe.scores))
+    return torch.utils.data.DataLoader(l, batch_size=batch_size, shuffle=(not test))
+
+def augment_dataset(original, *score_lambdas):
+    to_concat = [original]
+    for i in score_lambdas:
+        to_concat+= [original[i(original.scores)]]
+    return pd.concat(to_concat)
+
+
+class BERThoven(nn.Module):
+    def __init__(self, bert_model):
+        super(BERThoven, self).__init__()
+        self.bert_layers = bert_model
+        bert_out_features = self.bert_layers.pooler.dense.out_features
+        # self.lin_layer = nn.Linear(bert_out_features*2, 1)
+        self.lin_layer = nn.Linear(bert_out_features, 1)
+
+        self.droupout_layer = nn.Dropout(p=0.5)
+
+    def forward(self, x1, x2):
+        #        self.bert_layers.eval()
+        #        with torch.no_grad():
+        out1a = self.bert_layers(x1)[1]
+        # out1b = self.bert_layers(x2)[1]
+
+        # Using position 1 for the pooled head
+
+        # out1x = torch.cat((out1a,out1b),1)
+        out1x = out1a  # + out1b
+
+        out2 = self.droupout_layer(out1x);
+        out3 = self.lin_layer(out2)
+        return out3.squeeze()
+
+
+def check_accuracy(loader, model, max_sample_size=None):
+    model = model.to(device=device)
+    num_correct = 0
+    num_samples = 0
+    model.eval()  # set model to evaluation mode
+    abs_error = 0
+    sqr_error = 0
+
+    with torch.no_grad():
+        scores_epoch = []
+        truth_epoch = []
+
+        for x1, x2, y in loader:
+            truth_epoch += y.tolist()
+            x1 = x1.to(device=device, dtype=torch.long)
+            x2 = x2.to(device=device, dtype=torch.long)
+            y = y.to(device=device, dtype=torch.float32)
+            scores = model.forward(x1, x2)
+            scores_epoch += scores.tolist()
+
+            abs_error += (scores - y).abs().sum()
+            sqr_error += ((scores - y) ** 2).sum()
+            num_samples += scores.size(0)
+            if (max_sample_size != None and num_samples >= num_samples):
+                break;
+        mse = sqr_error / num_samples
+        mae = abs_error / num_samples
+        pr, _ = scipy.stats.pearsonr(scores_epoch, truth_epoch)
+
+        print('Mean Absolute Error: %.3f, Mean Squared Error %.3f, Pearson: %.3f' % (mse, mae, pr))
+
+
+def train_part(model, dataloader, optimizer, scheduler,
+               epochs=1, max_grad_norm=1.0, print_every=75,
+               loss_function=F.mse_loss):
+    # see F.smooth_l1_loss
+
+    avg_loss = 1
+    momentum = 0.05
+
+    model = model.to(device=device)  # move the model parameters to CPU/GPU
+    for e in range(epochs):
+        print(f"Iterations per epoch:{len(dataloader)}")
+        for t, (x1, x2, y) in enumerate(dataloader):
+            model.train()  # put model to training mode
+            x1 = x1.to(device=device, dtype=torch.long)
+            x2 = x2.to(device=device, dtype=torch.long)
+            y = y.to(device=device, dtype=torch.float32)
+
+            scores = model(x1, x2)
+
+            loss = loss_function(scores, y)
+            # Zero out all of the gradients for the variables which the optimizer
+            # will update.
+
+            optimizer.zero_grad()
+
+            loss.backward()
+
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
+            optimizer.step()
+
+            # Effectively doubling the batch size
+            # if t%2 ==0:
+            #   torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
+            #   optimizer.step()
+            #   optimizer.zero_grad()
+
+            scheduler.step()
+            l = loss.item()
+
+            avg_loss = l * momentum + avg_loss * (1 - momentum)
+
+            if t % print_every == 0:
+                print()
+                print('Epoch: %d, Iteration %d, loss = %.4f, avg_loss = %.4f' % (e, t, l, avg_loss), end="")
+            print(".", end="")
+        print()
+        print("Avg loss %.3f" % (avg_loss))
+        print("Checking accuracy on dev:")
+        check_accuracy(dataLoader_dev, model)
+        # print("Saving the model.")
+        # torch.save(model.state_dict(), 'nlp_model.pt')
+
+
+def get_test_labels(loader, model):
+    model = model.to(device=device)
+    num_correct = 0
+    num_samples = 0
+    model.eval()  # set model to evaluation mode
+    abs_error = 0
+    sqr_error = 0;
+    all_scores = []
+    with torch.no_grad():
+        for x1, x2 in loader:
+            x1 = x1.to(device=device, dtype=torch.long)
+            x2 = x2.to(device=device, dtype=torch.long)
+            scores = model.forward(x1,x2)
+            all_scores += [i.item() for i in scores]
+    return all_scores

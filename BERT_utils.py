181fa2403be0f67887d498604e12fd79c7450241
Guilherme Freire <guilhermebfreire@gmail.com>
2020-02-28 08:11:05 +0000

    Started adding comments to the files

--- BERT_utils.py
+++ BERT_utils.py
@@ -16,10 +16,17 @@ from transformers import (
 )
 from utils import add_mask, pad, prepro_df
 
+# Tokenizer from pretrained model
 tokenizer = AutoTokenizer.from_pretrained("bert-base-multilingual-cased")
 
 
 def get_new_bert_model():
+    """
+    Returns a new BERT model with guaranteed freshly loaded weights.
+    Avoids retraining models on top of others if you're not careful
+    """
+
+    # If the model isn't downloaded, download it and save it to disk
     if not os.path.exists("bert_weights"):
         os.mkdir("bert_weights")
         bm = AutoModel.from_pretrained(
@@ -28,6 +35,7 @@ def get_new_bert_model():
         bm.save_pretrained("./bert_weights/")
         torch.save(bm.state_dict(), "./bert_weights/bert-base-untrained.pth")
         print("Bert Model downloaded.")
+    # Otherwise, simply load it from disk
     else:
         bm = AutoModel.from_pretrained("./bert_weights/")
         bm.load_state_dict(torch.load("./bert_weights/bert-base-untrained.pth"))
@@ -38,7 +46,12 @@ def get_new_bert_model():
 
 
 def is_model_new(bm: AutoModel):
+    """Verifies if the BERT model has fresh weights, i.e. hasn't been trained yet.
+    bm: AutoModel object representing the BERT model
+    """
     l = list(bm.parameters())
+    # Check two arbitrary weights for equality
+    # We simply check two weights for computational efficiency
     return (
         l[6][13].item() == -0.11790694296360016
         and l[-5][10].item() == -0.015535828657448292
@@ -46,12 +59,25 @@ def is_model_new(bm: AutoModel):
 
 
 class BERTHovenDataset(Dataset):
+    """Class responsible of handling pre-processing of data handed to BERT
+    """
+
     def __init__(self, dataframe, test=False):
-        self.samples = []
-        self.test = test
+        """
+        dataframe: pandas.DataFrame object containing the dataset
+        test: boolean values describing whether or not this is the test set
+        """
+        self.samples = []  # Will store the final data
+        self.test = test  # Whether or not the dataset is a test set
+
+        # Convert the strings to token indices
         input1, input2 = get_tokenized(dataframe)
+
+        # Pad data according to the longest sentence
         x1, x1_mask = pad(input1)
         x2, x2_mask = pad(input2)
+
+        # Build the tuple structure to be used in training and testing
         for idx, _ in enumerate(tqdm(range(len(x1)), desc="Loading Data", leave=False)):
             sample = {
                 "x1": x1[idx],
@@ -59,14 +85,20 @@ class BERTHovenDataset(Dataset):
                 "x2": x2[idx],
                 "x2_mask": x2_mask[idx],
             }
+
+            # The score feature is only available for test and dev sets
             if not self.test:
                 sample["score"] = dataframe.iloc[idx].scores
             self.samples.append(sample)
 
     def __len__(self):
+        """Returns the size of the dataset"""
         return len(self.samples)
 
     def __getitem__(self, item):
+        """Returns a tuple to be used in an iteration of training or testing
+        item: index of the row to select the tuple from
+        """
         sample = self.samples[item]
         x1 = sample["x1"]
         x1_mask = sample["x1_mask"]
@@ -79,26 +111,50 @@ class BERTHovenDataset(Dataset):
 
 
 class MaskedDataset(BERTHovenDataset):
-    """Dataset for image segmentation."""
+    """Class responsible of handling pre-processing of data handed to BERT.
+       This modification allows for words in the sentence to randomly be
+       replaced by [MASK] tokens.
+       This can be seen as a way to introduce noise into the system.
+    """
 
-    def __init__(self, dataframe, number_of_mask=1, test=False):
+    def __init__(self, dataframe, number_of_masks=1, test=False):
+        """
+        dataframe: pandas.DataFrame object containing the dataset
+        number_of_masks: Amount of words to substitute for masks in the sentence
+        test: boolean values describing whether or not this is the test set
+        """
         super().__init__(dataframe, test)
+
+        # Special tokens shouldn't be replaced by mask
         self.no_replace = [104, 102, 103, 0]  # MASK CLS SEP PAS
-        self.number_of_mask = number_of_mask
+        self.number_of_mask = number_of_masks  # Saves the number of masks
 
     def __getitem__(self, item):
+        """Returns a tuple with masked sentences to be used in an iteration of
+        training or testing
+        item: index of the row to select the tuple from
+        """
+
+        # Select appropriate row according to `item`
         sample = self.samples[item]
+
+        # Adds masks to the original sentence
         x1 = self.add_mask(sample["x1"])
         x1_mask = sample["x1_mask"]
         x2 = self.add_mask(sample["x2"])
         x2_mask = sample["x2_mask"]
 
+        # Return tuple
         if not self.test:
             score = sample["score"]
             return x1, x1_mask, x2, x2_mask, score
         return x1, x1_mask, x2, x2_mask
 
     def add_mask(self, x):
+        """Randomly substitutes tokens for masks according to a predefinied
+        number of masks
+        x: List of indices representing the original sentence
+        """
         for k in range(self.number_of_mask):
             index_mask = np.random.randint(0, len(x) - 1)
             while x[index_mask] in self.no_replace:
@@ -109,84 +165,150 @@ class MaskedDataset(BERTHovenDataset):
 
 
 def get_tokenized(dataframe):
+    """Performs tokenizatoin and index substitution on a dataframe
+    This function also concatenates the sentences in both ways, appending a
+    [SEP] token after each one.
+
+    dataframe: pandas.DataFrame object containing the dataset
+    """
     input1 = (
         dataframe.apply(
-            lambda a: "[CLS] " + a.src + " [SEP] " + a.mt + " [SEP]", axis=1
+            # Source language followed by translation
+            lambda a: "[CLS] " + a.src + " [SEP] " + a.mt + " [SEP]",
+            axis=1,
         )
+        # First we tokenize
         .apply(lambda a: tokenizer.tokenize(a))
+        # Then we substitute indices
         .apply(lambda a: tokenizer.convert_tokens_to_ids(a))
     )
 
     input2 = (
         dataframe.apply(
-            lambda a: "[CLS] " + a.mt + " [SEP] " + a.src + " [SEP]", axis=1
+            # Translation followed by source language
+            lambda a: "[CLS] " + a.mt + " [SEP] " + a.src + " [SEP]",
+            axis=1,
         )
-        .apply(lambda a: tokenizer.tokenize(a))
-        .apply(lambda a: tokenizer.convert_tokens_to_ids(a))
+        .apply(lambda a: tokenizer.tokenize(a))  # Tokenize
+        .apply(lambda a: tokenizer.convert_tokens_to_ids(a))  # Substitute indices
     )
     return input1, input2
 
 
 def get_tokenized_with_mask(dataframe):
+    """Performs tokenization, masking and index substitution on a dataframe
+    This function concatenates the sentences in both ways, appending a
+    [SEP] token after each one.
+    After that it applies random masking to the previous result.
+    Finally the tokens are substituted for their indices
+
+    dataframe: pandas.DataFrame object containing the dataset
+    """
     input1 = (
         dataframe.apply(
-            lambda a: "[CLS] " + a.src + " [SEP] " + a.mt + " [SEP]", axis=1
+            # Source language followed by translation
+            lambda a: "[CLS] " + a.src + " [SEP] " + a.mt + " [SEP]",
+            axis=1,
         )
-        .apply(lambda a: tokenizer.tokenize(a))
-        .apply(lambda a: add_mask(a))
-        .apply(lambda a: tokenizer.convert_tokens_to_ids(a))
+        .apply(lambda a: tokenizer.tokenize(a))  # Tokenize
+        .apply(lambda a: add_mask(a))  # Random masking
+        .apply(lambda a: tokenizer.convert_tokens_to_ids(a))  # Index substitution
     )
 
     input2 = (
         dataframe.apply(
-            lambda a: "[CLS] " + a.mt + " [SEP] " + a.src + " [SEP]", axis=1
+            # Translation followed by source language
+            lambda a: "[CLS] " + a.mt + " [SEP] " + a.src + " [SEP]",
+            axis=1,
         )
-        .apply(lambda a: tokenizer.tokenize(a))
-        .apply(lambda a: add_mask(a))
-        .apply(lambda a: tokenizer.convert_tokens_to_ids(a))
+        .apply(lambda a: tokenizer.tokenize(a))  # Tokenize
+        .apply(lambda a: add_mask(a))  # Random masking
+        .apply(lambda a: tokenizer.convert_tokens_to_ids(a))  # Index substitution
     )
 
     return input1, input2
 
 
 def get_data_loader(dataframe, batch_size=32, test=False, preprocessor=None, fit=False):
+    """Returns a Torch DataLoader object containing the dataset
+    dataframe: pandas.DataFrame object containing the dataset
+    batch_size: Size of the batches
+    test: boolean value describing whether or not this is the test set
+    preprocessor: A custom function to perform pre-processing on the data
+    fit: Boolean value describing whether or not to fit the preprocessor to the data
+    """
+    # Start by pre-processing the data
     dataframe = prepro_df(dataframe, preprocessor, fit)
+
+    # Create an instance of the dataset wrapper
     ds = BERTHovenDataset(dataframe, test=test)
+
+    # Return a Torch compatible DataLoader
     return torch.utils.data.DataLoader(ds, batch_size=batch_size, shuffle=(not test))
 
 
 def get_data_loader_masked(
     dataframe, batch_size=32, test=False, preprocessor=None, fit=False
 ):
+    """Returns a Torch DataLoader object containing the dataset
+    This DataLoader applies random masking to its data
+    dataframe: pandas.DataFrame object containing the dataset
+    batch_size: Size of the batches
+    test: boolean value describing whether or not this is the test set
+    preprocessor: A custom function to perform pre-processing on the data
+    fit: Boolean value describing whether or not to fit the preprocessor to the data
+    """
+    # Start by pre-processing the data
     dataframe = prepro_df(dataframe, preprocessor, fit)
+
+    # Create an instance of the masking capable dataset wrapper
     masked_df = MaskedDataset(dataframe)
+
+    # Return a Torch compatible DataLoader
     return torch.utils.data.DataLoader(
         masked_df, batch_size=batch_size, shuffle=(not test)
     )
 
 
 def get_sentence_embeddings(dataframe, bert_model, device, test=False, batch_size=32):
+    """Returns the embeddings of a sentence according to a given BERT model
+    dataframe: pandas.DataFrame object containing the dataset
+    bert_model BERT model used to generate the embeddings
+    device: The device to use. Either "cuda" or "cpu"
+    test: boolean value describing whether or not this is the test set
+    batch_size: Size of the batches
+    """
     print("Tokenizing data...")
+    # Start by tokenizeng the sentences
     input1, input2 = get_tokenized(dataframe)
 
+    # Create a custom DataLoader to serve the sentence pair in question
     loader = torch.utils.data.DataLoader(list(zip(input1, input2)), batch_size=32)
 
+    # Set model to evaluation mode
     bert_model.eval()
+
+    # Set the model to the appropriate device
     bert_model.to(device=device)
 
-    l = []
-    z = len(list(loader))
+    embeddings = []  # Will hold the embeddings
 
+    # Deactivate gradients for evaluation
     with torch.no_grad():
+        # Iterate over the temporary DataLoader to retrieve sentences
         for i, (x1, x2) in enumerate(loader):
+            # Generate embeddings
             x1 = torch.LongTensor(pad(x1)).to(device=device, dtype=torch.long)
             x2 = torch.LongTensor(pad(x2)).to(device=device, dtype=torch.long)
             o1 = bert_model(x1)[1]
             o2 = bert_model(x2)[1]
 
             out = [(o1[i], o2[i]) for i in range(len(o1))]
-            l += out
+            embeddings += out  # Store embeddings
 
     if not test:
-        l = list(zip(l, dataframe.scores))
-    return torch.utils.data.DataLoader(l, batch_size=batch_size, shuffle=(not test))
+        embeddings = list(zip(embeddings, dataframe.scores))
+    # Return a new DataLoader containing the embeddings
+    return torch.utils.data.DataLoader(
+        embeddings, batch_size=batch_size, shuffle=(not test)
+    )
--- comparer_model.py
+++ comparer_model.py
@@ -4,11 +4,15 @@ from torch import nn
 
 
 class EncoderRNN(nn.Module):
+    """
+    Architecture for the encoder
+    """
+
     def __init__(self, vocab_size, hidden_size):
         super(EncoderRNN, self).__init__()
-        self.hidden_size = hidden_size
-        self.embedding = nn.Embedding(vocab_size, hidden_size)
-        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.hidden_size = hidden_size  # Dimensions of the hidden state (memory vector)
+        self.embedding = nn.Embedding(vocab_size, hidden_size)  # Embedding layer
+        self.gru = nn.GRU(hidden_size, hidden_size)  # Recurrent layer
 
     def forward(self, _input, hidden):
         embedded = self.embedding(_input).view(1, _input.shape[0], -1)
@@ -21,21 +25,35 @@ class EncoderRNN(nn.Module):
 
 
 class AttnDecoderRNN(nn.Module):
+    """
+        Architecture for the decoder
+        """
+
     def __init__(self, vocab_size, hidden_size, max_length, dropout_p=0.1):
         super(AttnDecoderRNN, self).__init__()
-        self.hidden_size = hidden_size
-        self.vocab_size = vocab_size
-        self.dropout_p = dropout_p
-        self.max_length = max_length
+        self.hidden_size = hidden_size  # Dimensions of the hidden state (memory vector)
+        self.vocab_size = vocab_size  # Number of words in the vocabulary
+        self.dropout_p = dropout_p  # Dropout probability
+        self.max_length = max_length  # Maximum length of a sentence
 
-        self.embedding = nn.Embedding(self.vocab_size, self.hidden_size)
-        self.attn = nn.Linear(self.hidden_size * 2, self.max_length)
-        self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
-        self.dropout = nn.Dropout(self.dropout_p)
-        self.gru = nn.GRU(self.hidden_size, self.hidden_size)
-        self.out = nn.Linear(self.hidden_size, 1)
+        self.embedding = nn.Embedding(
+            self.vocab_size, self.hidden_size
+        )  # Embedding layer
+        self.attn = nn.Linear(
+            self.hidden_size * 2, self.max_length
+        )  # Attention generation layer
+        self.attn_combine = nn.Linear(
+            self.hidden_size * 2, self.hidden_size
+        )  # Attention downscaling layer
+        self.dropout = nn.Dropout(self.dropout_p)  # Dropout layer
+        self.gru = nn.GRU(self.hidden_size, self.hidden_size)  # Recurrent layer
+        self.out = nn.Linear(self.hidden_size, 1)  # Final output generation layer
 
     def forward(self, _input, hidden, encoder_outputs):
+        """
+        Attention forward pass implemented according to Bahdanau et al on "NEURAL MACHINE TRANSLATION
+        BY JOINTLY LEARNING TO ALIGN AND TRANSLATE"
+        """
         embedded = self.embedding(_input).view(1, _input.shape[0], -1)
         embedded = self.dropout(embedded)
 
